44022598315b35df9014d207a3fc294321150d2e
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-20 15:21:14 -0700

    Changed fatalError to a throw call in IRI.

--- Sources/SPARQLSyntax/IRI.swift
+++ Sources/SPARQLSyntax/IRI.swift
@@ -12,39 +12,49 @@ public class IRI : Codable {
     public let absoluteString: String
 
     public init?(string: String) {
-        var uri = SERD_URI_NULL
-        let absolute = withUnsafeMutablePointer(to: &uri) { (u) -> String? in
-            guard let stringData = string.cString(using: .utf8) else { return nil }
-            return stringData.withUnsafeBytes { (bytes) -> String? in
-                let stringPtr = bytes.bindMemory(to: UInt8.self)
-                let status = serd_uri_parse(stringPtr.baseAddress, u)
-                if status == SERD_SUCCESS {
-                    return u.pointee.value
-                } else {
-                    return nil
+        do {
+            var uri = SERD_URI_NULL
+            let absolute = try withUnsafeMutablePointer(to: &uri) { (u) throws -> String? in
+                guard let stringData = string.cString(using: .utf8) else { return nil }
+                return try stringData.withUnsafeBytes { (bytes) throws -> String? in
+                    let stringPtr = bytes.bindMemory(to: UInt8.self)
+                    let status = serd_uri_parse(stringPtr.baseAddress, u)
+                    if status == SERD_SUCCESS {
+                        return try u.pointee.value()
+                    } else {
+                        return nil
+                    }
                 }
             }
-        }
-        if let a = absolute {
-            absoluteString = a
-        } else {
+            if let a = absolute {
+                absoluteString = a
+            } else {
+                return nil
+            }
+        } catch {
+            print("IRI error: \(error)")
             return nil
         }
     }
     
-    public init(fileURLWithPath path: String) {
-        var uri = SERD_URI_NULL
-        absoluteString = withUnsafeMutablePointer(to: &uri) { (u) in
-            let uu = UnsafeMutablePointer<SerdURI>(u)
-            let data = path.data(using: .utf8)!
-            var node = data.withUnsafeBytes { (s) in
-                serd_node_new_file_uri(s, nil, uu, false)
+    public init?(fileURLWithPath path: String) {
+        do {
+            var uri = SERD_URI_NULL
+            absoluteString = try withUnsafeMutablePointer(to: &uri) { (u) throws -> String in
+                let uu = UnsafeMutablePointer<SerdURI>(u)
+                let data = path.data(using: .utf8)!
+                var node = data.withUnsafeBytes { (s) in
+                    serd_node_new_file_uri(s, nil, uu, false)
+                }
+                let absolute = try u.pointee.value()
+                withUnsafeMutablePointer(to: &node) { (n) in
+                    serd_node_free(n)
+                }
+                return absolute
             }
-            let absolute = u.pointee.value
-            withUnsafeMutablePointer(to: &node) { (n) in
-                serd_node_free(n)
-            }
-            return absolute
+        } catch {
+            print("IRI error: \(error)")
+            return nil
         }
     }
     
@@ -67,11 +77,11 @@ public class IRI : Codable {
                         guard status == SERD_SUCCESS else { return nil }
 //                        print("Base IRI: \(b.pointee.value)")
                         var uri = SERD_URI_NULL
-                        return withUnsafeMutablePointer(to: &uri) { (out) -> String in
+                        return withUnsafeMutablePointer(to: &uri) { (out) -> String? in
                             let rr = UnsafePointer(r)
                             let bb = UnsafePointer(b)
                             serd_uri_resolve(rr, bb, out)
-                            let absolute = out.pointee.value
+                            let absolute = try? out.pointee.value()
                             return absolute
                         }
                     }
@@ -91,6 +101,10 @@ public class IRI : Codable {
     }
 }
 
+public enum IRIError : Error {
+    case encodingError(String)
+}
+
 fileprivate extension SerdChunk {
     var defined : Bool {
         if let _ = self.buf {
@@ -100,12 +114,12 @@ fileprivate extension SerdChunk {
         }
     }
     
-    var value : String {
+    func value() throws -> String {
         if let buf = self.buf {
             var data = Data()
             data.append(buf, count: self.len)
             guard let string = String(data: data, encoding: .utf8) else {
-                fatalError("Failed to turn \(self.len) bytes from SerdChunk into string")
+                throw IRIError.encodingError("Failed to turn \(self.len) bytes from SerdChunk into string: <\(data)>")
             }
             return string
         } else {
@@ -115,22 +129,22 @@ fileprivate extension SerdChunk {
 }
 
 fileprivate extension SerdURI {
-    var value : String {
+    func value() throws -> String {
         var value = ""
-        value += self.scheme.value
+        value += try self.scheme.value()
         value += ":"
         if self.authority.defined {
             value += "//"
         }
-        value += self.authority.value
-        value += self.path_base.value
-        value += self.path.value
-        let query = self.query.value
+        value += try self.authority.value()
+        value += try self.path_base.value()
+        value += try self.path.value()
+        let query = try self.query.value()
         if query.count > 0 {
             value += "?"
-            value += self.query.value
+            value += try self.query.value()
         }
-        value += self.fragment.value
+        value += try self.fragment.value()
         return value
     }
 }
