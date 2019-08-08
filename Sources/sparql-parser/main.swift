5786f85173badac2523607b035223bfe86af5e23
Gregory Williams <gwilliams@getty.edu>
2019-08-08 15:41:21 -0700

    Add default prefixes when running in wikidata mode.

--- Sources/sparql-parser/main.swift
+++ Sources/sparql-parser/main.swift
@@ -145,7 +145,44 @@ if let op = args.next() {
         
         guard let qfile = args.next() else { fatalError("No query file given") }
         do {
-            let (sparql, base) = try data(fromFileOrString: qfile)
+            let (sparqldata, base) = try data(fromFileOrString: qfile)
+            var sparql = sparqldata
+            if op == "wikidata" {
+                let prefixes = """
+                    PREFIX wd: <http://www.wikidata.org/entity/>
+                    PREFIX wds: <http://www.wikidata.org/entity/statement/>
+                    PREFIX wdv: <http://www.wikidata.org/value/>
+                    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+                    PREFIX wikibase: <http://wikiba.se/ontology#>
+                    PREFIX p: <http://www.wikidata.org/prop/>
+                    PREFIX ps: <http://www.wikidata.org/prop/statement/>
+                    PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    PREFIX bd: <http://www.bigdata.com/rdf#>
+
+                    PREFIX wdref: <http://www.wikidata.org/reference/>
+                    PREFIX psv: <http://www.wikidata.org/prop/statement/value/>
+                    PREFIX psn: <http://www.wikidata.org/prop/statement/value-normalized/>
+                    PREFIX pqv: <http://www.wikidata.org/prop/qualifier/value/>
+                    PREFIX pqn: <http://www.wikidata.org/prop/qualifier/value-normalized/>
+                    PREFIX pr: <http://www.wikidata.org/prop/reference/>
+                    PREFIX prv: <http://www.wikidata.org/prop/reference/value/>
+                    PREFIX prn: <http://www.wikidata.org/prop/reference/value-normalized/>
+                    PREFIX wdno: <http://www.wikidata.org/prop/novalue/>
+                    PREFIX wdata: <http://www.wikidata.org/wiki/Special:EntityData/>
+
+                    PREFIX schema: <http://schema.org/>
+                    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                    PREFIX prov: <http://www.w3.org/ns/prov#>
+                    PREFIX bds: <http://www.bigdata.com/rdf/search#>
+                    PREFIX gas: <http://www.bigdata.com/rdf/gas#>
+                    PREFIX hint: <http://www.bigdata.com/queryHints#>
+                """.data(using: .utf8)!
+                sparql = prefixes + sparqldata
+            }
             guard var p = SPARQLParser(data: sparql, base: base) else { fatalError("Failed to construct SPARQL parser") }
             if op == "wikidata" {
                 p.parseSPARQLStarReificationForWikidata = true
