75280dab885575a0bb836f740bcf9c86f7313141
Gregory Todd Williams <greg@evilfunhouse.com>
2020-05-28 13:39:51 -0700

    Add protocol support for parsing quads.

--- Sources/SPARQLSyntax/RDFSerialization.swift
+++ Sources/SPARQLSyntax/RDFSerialization.swift
@@ -8,11 +8,16 @@ public protocol RDFSerializer {
 }
 
 public typealias TripleHandler = (Term, Term, Term) -> Void
+public typealias QuadHandler = (Term, Term, Term, Term) -> Void
+
 public protocol RDFPushParser {
     init()
     var mediaTypes: Set<String> { get }
     func parse(string: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> Int
     func parseFile(_ filename: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> Int
+
+    func parse(string: String, mediaType: String, defaultGraph: Term, base: String?, handleQuad: @escaping QuadHandler) throws -> Int
+    func parseFile(_ filename: String, mediaType: String, defaultGraph: Term, base: String?, handleQuad: @escaping QuadHandler) throws -> Int
 }
 public typealias RDFParser = RDFPushParser
 
@@ -21,5 +26,8 @@ public protocol RDFPullParser {
     var mediaTypes: Set<String> { get }
     func parse(string: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> AnySequence<Triple>
     func parseFile(_ filename: String, mediaType: String, base: String?, handleTriple: @escaping TripleHandler) throws -> AnySequence<Triple>
+
+    func parse(string: String, mediaType: String, defaultGraph: Term, base: String?, handleQuad: @escaping QuadHandler) throws -> AnySequence<Quad>
+    func parseFile(_ filename: String, mediaType: String, defaultGraph: Term, base: String?, handleQuad: @escaping QuadHandler) throws -> AnySequence<Quad>
 }
 
